a2418c310467f81116701f726e9f0929dfe42d12
Joanne Basa <joannegbasa@gmail.com>
2017-11-02 08:46:16 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -18,12 +18,10 @@ function crazy() {
 
 function sayMyName() {
   // fix the code in here:
-  var name = "Cricky";
-
-  sayMy();
-
+  // var name = "Cricky";
   function sayMy() {
     console.log(name);
     var name = "Kristin";
   }
+  sayMy();
 }
